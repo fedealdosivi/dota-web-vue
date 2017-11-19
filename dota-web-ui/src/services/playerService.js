733627efff5ba6030d22b0537ec4823e17db1a8c
Federico Palomero Garcia <fedepalo13@gmail.com>
2017-11-18 21:41:03 -0300

    new promises added to the player service

--- dota-web-ui/src/services/playerService.js
+++ dota-web-ui/src/services/playerService.js
@@ -9,5 +9,46 @@ export default {
 	getPlayerById(playerId){
 		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId);
 		return promise;
-	}
+	},
+
+	getWinLosesByPlayerId(playerId){
+		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId +'/wl');
+		return promise;
+	},
+
+	getRecentMatchesByPlayerId(playerId){
+		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId +'/recentMatches');
+		return promise;
+	},
+
+	getMatchesByPlayerId(playerId){
+		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId +'/matches');
+		return promise;
+	},
+
+	getWinLosesByPlayerIdPerHeroe(playerId){
+		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId +'/heroes');
+		return promise;
+	},
+
+	getPeersByPlayerId(playerId){
+		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId +'/peers');
+		return promise;
+	},
+
+	getTotalStatsByPlayerId(playerId){
+		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId +'/totals');
+		return promise;
+	},
+
+	getCountsByPlayerId(playerId){
+		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId +'/counts');
+		return promise;
+	},
+
+	getWordsByPlayerId(playerId){
+		const promise=axios.get('https://api.opendota.com/api/players/'+ playerId +'/wordcloud');
+		return promise;
+	},
+
 }
