0684b643f5e00b5d44eed97537b9f8b224c840a1
Federico Palomero Garcia <fedepalo13@gmail.com>
2017-11-21 20:39:57 -0300

    added path to player search

--- dota-web-ui/src/utils/router.js
+++ dota-web-ui/src/utils/router.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import homeMenu from '../components/home-menu.vue'
 import allTeams from '../components/all-teams.vue'
+import playerSearch from '../components/player-search.vue'
 import playerProfile from '../components/player-profile.vue'
 import playerPeers from '../components/player-peers.vue'
 import playerRecent from '../components/player-recent.vue'
@@ -8,8 +9,9 @@ import playerRecent from '../components/player-recent.vue'
 
 export default new VueRouter({
   routes: [
-      { path: '/', component: homeMenu },
+      {path: '/', component: homeMenu },
       {path: "/teams/", component: allTeams},
+      {path: "/player/", component: playerSearch},   
       {path: "/player/:id", component: playerProfile},
       {path: "/player/:id/peers", component: playerPeers},
       {path: "/player/:id/rmatches", component: playerRecent}
